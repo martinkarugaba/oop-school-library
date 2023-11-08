b06ce13e638899599eec8157e90b1377cf93dc5f
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-08 19:41:36 +0300

    Refactor student.rb and fix linter errors

--- student.rb
+++ student.rb
@@ -1,27 +1,20 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
-
   def initialize(id, age, classroom = nil, parent_permission: true, name: 'Unknown')
     super(id, age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
-    add_to_classroom(classroom) if classroom
+    self.classroom = classroom
   end
 
   # Method to set the classroom and add the student to it
-  def classroom=(classroom)
-    @classroom = classroom
-    add_to_classroom(classroom)
-  end
+  def classroom=(new_classroom)
+    # Remove the student from the old classroom (if any)
+    @classroom&.students&.delete(self)
 
-  private
+    @classroom = new_classroom
 
-  # Helper method to add the student to a classroom
-  def add_to_classroom(classroom)
-    return unless classroom
-    
-    classroom.students << self unless classroom.students.include?(self)
+    # Add the student to the new classroom (if not nil)
+    new_classroom&.students&.push(self)
   end
 
   def play_hooky
