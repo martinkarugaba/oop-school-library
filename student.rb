8d361a86506f8edba5ca1b7ea881473bc9f0f1ef
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-08 13:19:15 +0300

    Add has-many/belongs-to relations

--- student.rb
+++ student.rb
@@ -5,7 +5,7 @@ class Student < Person
 
   def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
     super(id, age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
+    @classroom = nil
   end
 
   def play_hooky
