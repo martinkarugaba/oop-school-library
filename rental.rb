439e9b01e637a5bc40cd4e74c772138f8d373ae8
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-08 13:32:06 +0300

    Create many-to-may relationship btn person and book class

--- rental.rb
+++ rental.rb
@@ -1,7 +1,10 @@
 class Rental
   attr_accessor :date
+  attr_reader :person, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    @book = book
   end
 end
